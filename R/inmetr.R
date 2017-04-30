066d174784b1a7ca17b965009950ba6e407ad406
lhmet <lhmet@ufsm.br>
2017-04-30 02:17:27 -0300

    package help page

---
+++ R/inmetr.R
@@ -0,0 +1,17 @@
+#'	inmetr:	A	package	for	import data from Brazilian meteorological stations
+#'
+#'  The inmetr package offers a set of functions to import historical data from Brazilian meteorological stations available in the Meteorological Database for Education and Research \href{http://www.inmet.gov.br/projetos/rede/pesquisa}{BDMEP} of National Institute of Meteorology \href{http://www.inmet.gov.br}{INMET}
+#'	The main functions include:
+#'	import_bdmep,	bdmep_stations, bdmep_metadata, and data_description.
+#'
+#'	@section inmetr functions:
+#' \tabular{ll}{
+#'    \code{\link{import_bdmep}}  \cr
+#'    \code{\link{bdmep_stations}} \cr
+#'    \code{\link{bdmep_metadata}} \cr
+#'    \code{\link{data_description}} \cr
+#' }
+#'
+#'	@docType	package
+#'	@name	inmetr
+NULL
