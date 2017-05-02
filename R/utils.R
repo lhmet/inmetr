35b66e570f2f5e716742c022ff1198c036544206
lhmet <lhmet@ufsm.br>
2017-05-02 00:10:46 -0300

    management globalVariables

---
+++ R/utils.R
@@ -0,0 +1,15 @@
+utils::globalVariables(c(".", "data", "hora", "codigo", 
+                         "nome_estacao","prec", "site",
+                         "ws", "tcomp", "id"))
+
+#' Pipe operator
+#'
+#' See \code{\link[dplyr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+NULL
