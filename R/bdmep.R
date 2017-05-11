907435c293c57db3acb9fc2591a1ecf8a59390df
lhmet <lhmet@ufsm.br>
2017-05-11 16:23:46 -0300

    move code related to metadata from meteorological stations to new script metadata.R

--- R/bdmep.R
+++ R/bdmep.R
@@ -84,11 +84,10 @@ bdmep_read <- function(x){
                   data = NULL,
                   hora = NULL,
                   id = as.character(codigo),
-                  codigo = NULL,
-                  request_status = "sucessfull") 
+                  codigo = NULL) 
   # reorder columns
   bdmepd <- bdmepd %>% 
-    dplyr::select(date, id, prec:ws, -tcomp, request_status)
+    dplyr::select(date, id, prec:ws, -tcomp)
   
   # duplicated rows
     bdmepd <- dplyr::distinct(bdmepd)
@@ -170,7 +169,6 @@ bdmep_import_station <- function(.id = "83552" ,
             "station: " , .id)
   }
   # visualize(r)
-  gc()
   # step 2 - get data
   url_data <- "http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?&mRelEstacao=XXXXX&btnProcesso=serie&mRelDtInicio=dd/mm/yyyy&mRelDtFim=DD/MM/YYYY&mAtributos=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,"
   #url_data <- "http://www.inmet.gov.br/projetos/rede/pesquisa/gera_serie_txt.php?&mRelEstacao=83980&btnProcesso=serie&mRelDtInicio=01/01/1961&mRelDtFim=01/01/2017&mAtributos=1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,"
@@ -184,18 +182,16 @@ bdmep_import_station <- function(.id = "83552" ,
   # request data  
   r2 <- httr::GET(url_data)
   
+  msg <- httr::http_status(r2)$message
+  
   #httr::stop_for_status(r2)
   if (.verbose) {
-    if (httr::status_code(r2) == 200) {
-      message("Request data ok.", "\n")
-    } else {
-      httr::message_for_status(r2)  
+      httr::message_for_status(r2)
       cat("\n")
-    }
-  }  
+  }
   
+  # column to inform request status
   if (httr::status_code(r2) != 200) {
-    msg <- httr::http_status(r2)$message
     xtidy <- bdmep_template(.id , msg)
     return(xtidy)
   }
@@ -207,6 +203,9 @@ bdmep_import_station <- function(.id = "83552" ,
   
   # tidy data and output
   xtidy <- bdmep_read(x)
+  # column with status
+  xtidy <- dplyr::mutate(xtidy, request_status = msg)
+  
   return(xtidy)
 }
 
@@ -254,195 +253,16 @@ bdmep_import <- function(ids = c("83936", "83967") ,
                                            .verbose = verbose))
 } 
 
-##' Get metadata on meteorological stations
-##'
-##' Fetch the coordinates on meteorological stations from INMET
-##' \url{http://www.inmet.gov.br/projetos/rede/pesquisa/lista_estacao.php}
-##' 
-##' @importFrom dplyr %>%
-##' @return A data frame is returned with metadata, including the stations
-##'  \code{id}, and coordinates (\code{lon}, \code{lat}, \code{alt})
-##' @export
-##' @author Jonatan Tatsch
-##' @examples 
-##' 
-##' info <- bdmep_metadata()
-##' head(info)
-
-bdmep_metadata <- function(){
-  # omm id, lat, lon, alt
-  link_stns_info <- "http://www.inmet.gov.br/sim/sonabra/index.php"
-  
-  txt <- httr::GET(link_stns_info) %>%
-    httr::content('text') %>% 
-    textConnection() %>%
-    readLines() 
-  
-  closeAllConnections()
-  rm(link_stns_info)
-  
-  txt_subset <- 
-    txt %>%
-    stringi::stri_trans_general("latin-ascii") %>%
-    stringr::str_subset("Codigo OMM")
-  
-  txt_info_num <- txt_subset %>%
-    stringr::str_extract_all("[-+]?([0-9]*\\.[0-9]+|[0-9]+)")
-  
-  #rm(txt)
-  
-  tab_info <- 
-    txt_info_num %>%
-    purrr::map_df(function(x){
-      #x <- a
-      n <- length(x)
-      x[c(3, n - (2:0))] %>%
-        matrix(nrow = 1) %>%
-        dplyr::as_data_frame() %>%
-        return()
-    }) %>%
-    setNames(c("id", "lat", "lon", "alt")) %>%
-    dplyr::select_("id", "lon", "lat", "alt") %>%
-    dplyr::mutate_at(dplyr::vars(dplyr::one_of(c("lat", "lon", "alt"))), as.numeric) %>%
-    data.frame() 
-  
-  #tab_info <- dplyr::full_join(bdmep_stations(), tab_info, by = "id")
-  
-  return(tab_info)
-} # end function bdmep_metadata
-
-
-##' Get basic information on meteorological station from INMET
-##'
-##' This function is used to find the OMM station ID that can be
-##' used to import BDMEP data using \code{\link{bdmep_import}}
-##' @description Get OMM code, state and station name on meteorological stations from INMET
-##' \url{http://www.inmet.gov.br/projetos/rede/pesquisa/lista_estacao.php}
-##' @importFrom dplyr %>%
-##' @return a data frame is returned with 
-##'  \code{name}, \code{state}, \code{id}
-##' @export
-##' @author Jonatan Tatsch
-##' @examples 
-##' 
-##' \dontrun{
-##' # tyr get information from inmet web site
-##' stns <- bdmep_stations()
-##' head(stns, 15)
-##' #save(stns, file = "data/stns.rda")
-##' }
-bdmep_stations <- function(){
-  link_stns_l <- "http://www.inmet.gov.br/projetos/rede/pesquisa/lista_estacao.php"
-  tab <- httr::GET(link_stns_l) %>%
-    httr::content('text') %>%
-    xml2::read_html() %>%
-    rvest::html_node("table") %>%
-    rvest::html_table(header = TRUE)
-
-  ## remove colum named " "
-  tab <- tab %>%
-    names() %>%
-    stringr::str_trim(.) %>%
-    purrr::discard(str_empty) %>%
-    subset(tab, sel = .)
-  
-  tab <- tab %>%
-    names() %>%
-    tolower() %>%
-    # replace accented characters with non-accented counterpart
-    stringi::stri_trans_general("latin-ascii") %>%
-    stringr::str_replace_all(" ", "_") %>%
-    stringr::str_replace("_da", "") %>%
-    setNames(tab, nm = .) %>%
-    tidyr::separate(nome_estacao, c("nome", "estado"), sep = " - ")
-  tab <- tab %>% 
-    data.frame() %>%
-    setNames(c("name", "state", "id")) %>%
-    dplyr::mutate(id = as.character(id))
-    return(tab)
-}
-
-##' Description of meteorological variables 
-##'
-##' This function describe the Meteorological variables imported by \code{\link{bdmep_import}}
-##' @description Get variable names, description and units
-##' @importFrom dplyr %>%
-##' @details to information about instruments see \url{http://www.inmet.gov.br/portal/index.php?r=home/page&page=instrumentos}
-##' @return a data frame is returned with 
-##'  \code{varname}, \code{description}, \code{unit}
-##' @export
-##' @author Jonatan Tatsch
-##' @examples 
-##' met_vars <- bdmep_description()
-##' met_vars
-##' 
-bdmep_description <- function() {
-desc <- data.frame(varname = c("date", 
-                             "id", 
-                             "prec",  
-                             "tair", 
-                             "tw", 
-                             "tmax", 
-                             "tmin",  
-                             "urmax", 
-                             "patm", 
-                             "pnmm",  
-                              "wd",  
-                             "wsmax",    
-                             "n",   
-                             "cc", 
-                             "evap", 
-                             "ur",   
-                             "ws"),
-             description = c("date and time information",
-                             "station ID",
-                             "precipitation",
-                             "air temperature",
-                             "wet bulb temperature",
-                             "maximum air temperature",
-                             "minimum air temperature",
-                             "maximum relative humidity",
-                             "atmospheric pressure",
-                             "mean sea level atmospheric pressure",
-                             "wind direction",
-                             "wind gust",
-                             "sunshine hours",
-                             "cloud cover",
-                             "evaporation",
-                             "relative humidity",
-                             "wind speed"),
-                    unit = c("-",
-                             "-",
-                             "mm",
-                             "deg C",
-                             "deg C",
-                             "deg C",
-                             "deg C",
-                             "%",
-                             "hPa",
-                             "hPa",
-                             "deg",
-                             "m/s",
-                             "h",
-                             "-",
-                             "mm",
-                             "%",
-                             "m/s"),
-         stringsAsFactors = FALSE)
-    new_line <- dplyr::data_frame(varname = "request_status",
-                                  description = "Information on the status of a request",
-                                  unit = NA_character_)
-    desc <- dplyr::bind_rows(desc, new_line)
-      return(desc)
-}
-
 
 #' Template bdmep dataframe to be used when the status of a request was not successfully executed.
-#'
+#' 
+#' @details This is used when the status of a request 
+#' 
 #' @param .id a character scalar with the meteorological station code
 #' @param .req_status character scalar with information on the status of a request
+#' 
 #' @importFrom dplyr %>%
-#' @return a dataframe with values filled with NA, except for id and request_status
+#' @return a dataframe with variables filled with NA, except for id and request_status
 bdmep_template <- function(.id, .req_status){
   varnames <- bdmep_description()[, "varname"]
   templ_df <- as.data.frame(t(rep(NA, length(varnames))), stringsAsFactors = FALSE) 
