90f1ff13511f936bdf607803bc0984d822b286fd
lhmet <lhmet@ufsm.br>
2017-05-20 18:52:22 -0300

    adding bdmep_summary

--- R/utils.R
+++ R/utils.R
@@ -24,3 +24,12 @@ str_empty <- function(string) {
   string <- as.character(string)
   string == ""
 } 
+
+
+bdmep_summary <- function(x){
+  #x <- xtidy
+  x %>%
+    dplyr::group_by(id, day = lubridate::day(date))
+  
+}
+
