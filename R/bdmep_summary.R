8b4558d919e6298e5fb4e331a55d878f43222629
lhmet <lhmet@ufsm.br>
2017-05-27 22:24:18 -0300

    under construction

--- R/bdmep_summary.R
+++ R/bdmep_summary.R
@@ -1,10 +1,10 @@
-# x <- bdmep_import(id = c("83936"), 
+# x <- bdmep_import(id = c("82915","83235"),
 #                         sdate = "01/01/1961",
 #                          edate = format(Sys.Date(), '%d/%m/%Y'),
 #                          email = "your-email",
 #                          passwd = "your-password",
 #                          verbose = TRUE)
-                         
+
 
 
 mean_thresh <- function(x, thresh = 0) {
@@ -20,16 +20,57 @@ data_cap <- function(x, percent = FALSE){
   sum(!is.na(x))
 }
 
-bdmep_summary <- function(x, data_thresh = 0){
+bdmep_summary <- function(x, data_thresh = 0, meta_data = bdmep_meta){
+  # xo -> x
+  # remove estações indisponíveis (Bad Request (HTTP 400))
+  x <- tibble::as_tibble(x) %>%
+    dplyr::filter(., !is.na(date)) %>%
+    dplyr::select(., -request_status) 
+  
+  meta_data <- dplyr::filter(meta_data, id %in% unique(x$id)) %>%
+               dplyr::select(., dplyr::one_of(c("id", "time_zone", "offset_utc")))
+  
+  
+  x <- dplyr::full_join(x, meta_data, by = "id")
+  
+  lt2utc <- function(x, tzone) {
+    lubridate::with_tz(x, tzone[1]) %>%
+    lubridate::force_tz(., "UTC")
+  }
+  
+  x <- dplyr::group_by(x, id) %>%
+    dplyr::do(dplyr::mutate(., 
+                            #date_ = lt2utc(x = .$date,
+                            date = lt2utc(x = .$date,
+                                           tzone = unique(.$time_zone))
+                            )
+              ) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-offset_utc, -time_zone)
+  
+  # check <- dplyr::select(x, date, date_); head(check); tail(check)
+  # check <- dplyr::select(x, date); head(check); tail(check)
+  # a <- x %>% group_by(id, day = as.Date(date_)) %>% summarise(N = n()) %>% ungroup() %>% dplyr::rename("date" = day)
+  # timePlot(a, "N" ,type = "id")
+  # Amostragem irregular do INMET
+  x <- dplyr::rename(x, "site" = id)
+  y <- x %>%
+    dplyr::group_by(site, date = lubridate::as_date(date)) %>% 
+    dplyr::summarise(N = n()) %>%
+    dplyr::ungroup()
+  
+  ta <- timeAverage(x, avg.time = "day", statistic = "frequency", type = "site")
+  
+  z <- x %>%
+    dplyr::select(date, site, wd) %>%
+    dplyr::group_by(site, date = lubridate::as_date(date)) %>%
+    dplyr::summarise(N = n(), valid = data_cap(wd, percent = FALSE)) 
   
   # 1 regularizar series
   # 2 considerar TZ e obter para cada EST a data de acordo com o fuso horário local 
   # 3. agrupar por EST e DAY
   # 4. calcular a disp. de dados e de acordo com o limiar aplicar a média ou não
-  
-  #x <- xtidy
-  tzone <- attr(x$date, "tzone")
-  if (is.null(tzone)) tzone <- "UTC" 
+
   
   # complete dates 
   y <- x %>%
