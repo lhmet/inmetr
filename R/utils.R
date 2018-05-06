e20f0f4ec07ee6457084b64aae3ecbd333c8846d
lhmet <lhmet@ufsm.br>
2018-05-05 21:41:53 -0300

    new functions bdmep_write_csv and bdmep_completeness

--- R/utils.R
+++ R/utils.R
@@ -39,3 +39,63 @@ nvalid <- function(x) {
   # if(all(is.na(x))) return(0)
   sum(!is.na(x))
 }
+
+
+
+bdmep_write_csv <- function(data_bdmep = xtidy,
+                            folder = .destdir,
+                            na.strings = .na.strings,
+                            verbose = .verbose){
+  # if(!stringr::str_detect(.destfile, "\\.[a-z]{3,}")){
+  stopifnot(dir.exists(folder), 
+            all(c("date", "id", "request_status", "prec", "ws") %in% names(data_bdmep))
+            )
+  .id <- data_bdmep[1, "id"]
+  .file <- file.path(folder, paste0(.id, ".csv"))
+  
+  # readr::write_csv(x = dplyr::mutate(xtidy, date = as.character(date)),
+  readr::write_csv(
+    x = xtidy,
+    path = .file,
+    na = na.strings,
+    append = FALSE
+  )
+  
+  if(file.exists(.file)) {
+    if (verbose) message("Data saved in ", .file)
+    return(invisible(.file))
+  } else {
+    message("Cannot save data file ", .file)
+    return(invisible(NA_character_))
+  }
+}
+
+
+
+bdmep_completeness <- function(data_bdmep = xtidy){
+  
+  stopifnot(all(c("date", "id", "request_status", "prec", "ws") %in% names(data_bdmep)))
+  
+  out_summary <- data_bdmep %>%
+    dplyr::select(date, id, request_status) %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise(.,
+      sdate = min(date, na.rm = TRUE),
+      edate = max(date, na.rm = TRUE),
+      nrow = n(),
+      request_status = unique(request_status)
+    )
+  
+  out_data <- xtidy %>%
+    dplyr::group_by(id) %>%
+    dplyr::summarise_at(.,
+                        .vars = dplyr::vars(prec:ws),
+                        .funs = dplyr::funs(nvalid)
+    ) %>%
+    dplyr::full_join(out_summary, ., by = "id") %>%
+    dplyr::mutate_at(dplyr::vars(prec:ws),
+                     .funs = dplyr::funs(. / nrow * 100))
+  
+  return(out_data)
+}
+  
