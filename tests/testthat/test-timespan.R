fa64cbe46699fa0fed46e07cc491002efd595e79
lhmet <lhmet@ufsm.br>
2020-04-11 23:42:27 -0300

    check time span update

--- tests/testthat/test-timespan.R
+++ tests/testthat/test-timespan.R
@@ -1,3 +1,19 @@
 test_that("Requested time span not available in database", {
-  expect_equal(2 * 2, 4)
+  
+  # template data
+  id <- "83743" 
+  sdate <- format(Sys.Date()-90, "%d/%m/%Y")
+  edate <- format(Sys.Date(), "%d/%m/%Y")
+  msg_nodata_req <- .build_msg_nodata(id, sdate, edate)
+  data_ref <- bdmep_template(id, msg_nodata_req)
+  
+  # request data
+  data_req <- bdmep_import(id, sdate, edate,
+               email = "jdtatsch@gmail.com", 
+               passwd = "d17ulev5", 
+               verbose = FALSE
+               )
+  
+  expect_equal(data_req, data_ref)
+  
 })
